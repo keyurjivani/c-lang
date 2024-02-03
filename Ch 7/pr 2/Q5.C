408d0da3872effeb879dc30a446442bc98effc45
keyurjivani <keyurjivani07rw.com>
2024-02-03 13:04:43 +0530

    02/02/2024

--- Ch 7/pr 2/Q5.C
+++ Ch 7/pr 2/Q5.C
@@ -3,7 +3,7 @@
 main()
 {
 	int unit;
-	int bill, charge, total;
+	int bill, charge = 0, total = 0;
 	clrscr();
 
 	printf("Enter electicity units: ");
@@ -11,27 +11,27 @@ main()
 
 	if(unit <= 50)
 	{
-		total = (unit * 0.50)+unit ;
+		total = unit * 0.50;
 		printf("enter unit: %d",total);
 	}
 	else if(unit >= 51 || unit <= 150)
 	{
-		total = (unit * 0.75)+ unit ;
+		total = 50 * 0.50 + (unit - 50) * 0.75;
 		printf("Enter unit: %d",total);
 	}
-	else if(unit >= 151 || unit <= 250)
+	else if(unit >= 151 && unit <= 250)
 	{
-		total = (unit * 1.20)+ unit ;
+		total = 50 * 0.50 + 100 * 0.75 + (unit -150) * 1.20;
 	       printf("Enter unit: %d",total);
 	}
 	else if(unit >= 251)
 	{
-		total = (unit * 1.50) + unit;
+		total = 50 * 0.50 + 100 * 0.75 + 100 * 1.20 + (unit -250) * 1.50;
 		printf("Enter unit: %d",total);
 	}
 
-	//charge = total * 20 /100;
-	//total = total + charge;
-	//printf("Bill : %d",total);
+	charge = total * 20 /100;
+	total = total + charge;
+	printf("\nBill : %d",total);
 	getch();
 }
