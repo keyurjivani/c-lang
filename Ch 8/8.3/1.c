8af757fbaedfbfeb17df5af96a6d92aec6022f84
keyurjivani <keyurjivani07rw.com>
2024-03-19 11:37:42 +0530

    19/03

--- Ch 8/8.3/1.c
+++ Ch 8/8.3/1.c
@@ -4,14 +4,12 @@ int main() {
     int row, col,i,j;
     float sum = 0.0, average;
     
-  
     printf("Enter the array's row size: ");
     scanf("%d", &row);
     printf("Enter the array's column size: ");
     scanf("%d", &col);
     
     int array[row][col];
-    
     
     printf("\nEnter array's elements:\n");
     for( i = 0; i < row; i++) {
@@ -24,8 +22,6 @@ int main() {
     
     
     average = sum / (row * col);
-    
-    
     printf("\nAverage of an Array: %.2lf\n", average);
     
 }
